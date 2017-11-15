bd04404b7662b5404200086e64e3d637f7c7e11b
Stephan Schmidbauer <schmidbauer.stephan@gmail.com>
2017-11-15 17:11:33 +0100

    added a navbar and made some final changes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,7 +14,7 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.restaurant = Restaurant.find(params[:restaurant_id])
     if @review.save
-      redirect_to restaurant_path(@review)
+      redirect_to restaurant_path(@review.restaurant)
     else
       render :new
     end
